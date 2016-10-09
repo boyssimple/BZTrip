3aabe25782027911efb46924b9080ac50b8ed06a
luowei <luowei@pentaq.com>
2016-10-09 11:30:25 +0800

    afd

---
+++ BZTrip/BZTrip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BZTrip
+//
+//  Created by zhouMR on 16/10/9.
+//  Copyright © 2016年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
