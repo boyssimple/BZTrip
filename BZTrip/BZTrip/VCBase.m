ac1194da967e829d63c57adf3b4bbb786cae6d6d
luowei <luowei@pentaq.com>
2016-10-09 11:53:39 +0800

    架构设计

---
+++ BZTrip/BZTrip/VCBase.m
@@ -0,0 +1,22 @@
+//
+//  VCBase.m
+//  haochang
+//
+//  Created by zhouMR on 16/7/1.
+//  Copyright © 2016年 luowei. All rights reserved.
+//
+
+#import "VCBase.h"
+
+@interface VCBase ()
+
+@end
+
+@implementation VCBase
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
