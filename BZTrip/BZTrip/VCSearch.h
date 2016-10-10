19daa9fb3f615bdb16827da42042d8a5677b437a
luowei <luowei@pentaq.com>
2016-10-10 11:25:45 +0800

    ad

---
+++ BZTrip/BZTrip/VCSearch.h
@@ -0,0 +1,13 @@
+//
+//  VCSearch.h
+//  BZTrip
+//
+//  Created by zhouMR on 16/10/9.
+//  Copyright © 2016年 luowei. All rights reserved.
+//
+
+#import "VCBase.h"
+
+@interface VCSearch : VCBase
+
+@end
